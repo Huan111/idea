095608e512686002ca482b0d6a1111074db9d321
Huan111 <lgh1007115150@qq.com>
2022-08-19 18:49:24 +0800

    first commit

---
+++ module01/src/bean/Customer.java
@@ -0,0 +1,20 @@
+package bean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Customer {
+    public static void main(String[] args){
+        System.out.println("hello!!!");
+        String[] arr = new String[]{"Tom","lee","jetty"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        ArrayList list = new ArrayList<Integer>();
+        list.add(123);
+        list.add(456);
+        list.add(789);
+        
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Lee
+  Date: 2022/8/19
+  Time: 15:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>nihao</title>
+  </head>
+  <body>
+  <h1 style="color:red">hihi</h1>
+  </body>
+</html>
